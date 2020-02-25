610667be1ddf58a89808556205b3baf8b6fc810c
Lluismi <lmorey@cifpfbmoll.eu>
2020-02-25 03:20:29 +0100

    Añadido metodo dispatch

--- src/main/java/RicksyBussiness/UfosPark.java
+++ src/main/java/RicksyBussiness/UfosPark.java
@@ -5,22 +5,33 @@ import java.util.Map;
 
 public class UfosPark {
 	
-	private int fee = 500;
-	private Map<String, Integer> flota = new HashMap<String, Integer>();
+	private double fee = 500;
+	private Map<String, String> flota = new HashMap<String, String>();
 	
 	public UfosPark() {}
 
-	public int getFee() {
+	public double Fee() {
 		return this.fee;
 	}
 	
-	public Map<String, Integer> getFlota() {
+	public Map<String, String> getFlota() {
 		return this.flota;
 	}
 
 	public void add(String ovni) {
-		flota.put(ovni, 1);
-		flota.put(ovni, 2);
+		flota.put(ovni, ovni);
+		flota.put(ovni, ovni);
+	}
+	
+	public void dispatch(CreditCard abradolph) {
+		for (String ovni : flota.keySet()) {
+			if(flota.get(ovni) != null) {
+				abradolph.pay(fee); {
+					flota.put(ovni, abradolph.number());
+					break;
+				}
+			}
+		}
 	}
 	
 	public String getUfoOf(String f) {
