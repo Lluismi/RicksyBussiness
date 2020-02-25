007f8dbb5a4a61cf7afdadc0bf7ea1b6c9aaf22c
Lluismi <lmorey@cifpfbmoll.eu>
2020-02-25 12:12:22 +0100

    Refactorizado, dispatch(con error), hecho el getufo(con error), añadido el toString

--- src/main/java/RicksyBussiness/UfosPark.java
+++ src/main/java/RicksyBussiness/UfosPark.java
@@ -1,6 +1,8 @@
 package RicksyBussiness;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UfosPark {
@@ -10,32 +12,37 @@ public class UfosPark {
 	
 	public UfosPark() {}
 
-	public double Fee() {
-		return this.fee;
-	}
-	
-	public Map<String, String> getFlota() {
-		return this.flota;
-	}
-
 	public void add(String ovni) {
 		flota.put(ovni, ovni);
-		flota.put(ovni, ovni);
 	}
 	
+	/* Error abradolph paga 2 ovnis pero solo tiene 1*/
 	public void dispatch(CreditCard abradolph) {
 		for (String ovni : flota.keySet()) {
 			if(flota.get(ovni) != null) {
-				abradolph.pay(fee); {
+				abradolph.pay(fee);
 					flota.put(ovni, abradolph.number());
 					break;
-				}
+			}else if(flota.get(ovni) == abradolph.number()) {
+				break;
 			}
 		}
 	}
 	
-	public String getUfoOf(String f) {
-		return null;
+	/* Error le da el mismo ovni a todos  y morty le cobra aunque no haya ovni para el*/
+	public String getUfoOf(String number) {
+		String NameOvni = null;
+		for (String ovni : flota.keySet()) {
+			if (flota.get(ovni) == number) {
+				NameOvni = ovni;
+			}
+		}
+		return NameOvni;
 	}
 	
+	/* Error: Saca la lista de arriba abajo en vez de como pide la foto*/
+	public String toString() {
+		List<String> ovnis = new ArrayList<String>(flota.keySet());
+		return ovnis.toString();
+	}
 }
