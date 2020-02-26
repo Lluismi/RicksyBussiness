40af566f43825073935502535cb39ca7a8a5ff59
Lluismi <lmorey@cifpfbmoll.eu>
2020-02-26 02:37:55 +0100

    Añadidos metodos dispatch, stock y tostring, refactorizado getters y añadido comentarios

--- src/main/java/RicksyBussiness/CrystalExpender.java
+++ src/main/java/RicksyBussiness/CrystalExpender.java
@@ -2,19 +2,33 @@ package RicksyBussiness;
 
 public class CrystalExpender {
 	
-	private int stock = 0;
-	private double itemCost = 0;
+	private int stock = 3;
+	private double itemCost = 50;
+	
+	/* Constructor */
 	
 	public CrystalExpender(int stock, double itemCost) {
 		this.stock = stock;
 		this.itemCost = itemCost;
 	}
 	
-	public int getStock() {
-		return stock;
+	/* Comprueba si persona tiene dinero en credit
+	 * si tiene comprueba si hay los packs en stock
+	 * son mayores de 0 y resta 1 a stock */
+	public void dispatch(CreditCard persona) {
+		if (persona.pay(itemCost) && stock > 0) {
+			stock -= 1;
+		}
 	}
 	
-	public double getItemCost() {
-		return itemCost;
+	public int stock() {
+		return stock;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Stock: " + stock + "\n" +
+				"cost: " + itemCost;
 	}
 }
