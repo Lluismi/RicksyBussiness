c57d85d86c11853f221108459c150ae69303ebd7
Lluismi <lmorey@cifpfbmoll.eu>
2020-02-26 03:02:53 +0100

    Creada clase receptivo

---
+++ src/main/java/RicksyBussiness/Receptivo.java
@@ -0,0 +1,15 @@
+package RicksyBussiness;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receptivo {
+	
+	List<String> observers = new ArrayList<String>();
+	
+	/* Constructor */
+	
+	public Receptivo() {}
+	
+	
+}
