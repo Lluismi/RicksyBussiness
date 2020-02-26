806404a26ede31f1ac4960b93e37b2f4e6b228cb
Lluismi <lmorey@cifpfbmoll.eu>
2020-02-26 19:58:47 +0100

    espaciado añadido

--- src/main/java/RicksyBussiness/CrystalExpender.java
+++ src/main/java/RicksyBussiness/CrystalExpender.java
@@ -2,7 +2,7 @@ package RicksyBussiness;
 
 public class CrystalExpender implements GuestDispatcher {
 	
-	private int stock = 3;
+	private int stock = 0;
 	private double itemCost = 50;
 	
 	/* Constructor */
@@ -15,6 +15,7 @@ public class CrystalExpender implements GuestDispatcher {
 	/* Comprueba si persona tiene dinero en credit
 	 * si tiene comprueba si hay los packs en stock
 	 * son mayores de 0 y resta 1 a stock */
+	
 	public void dispatch(CreditCard persona) {
 		if (persona.pay(itemCost) && stock > 0) {
 			stock -= 1;
