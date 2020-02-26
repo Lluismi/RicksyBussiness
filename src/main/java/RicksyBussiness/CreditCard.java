03d4a6245a9e60480f4d4ed906a8de10d78d8285
Lluismi <lmorey@cifpfbmoll.eu>
2020-02-26 02:35:22 +0100

    Cambiado codigo del metodo pay

--- src/main/java/RicksyBussiness/CreditCard.java
+++ src/main/java/RicksyBussiness/CreditCard.java
@@ -7,24 +7,33 @@ public class CreditCard {
 	private double credit = 3000;
 	private final String SYMBOL = "EZI";
 	
+	/* Constructor */
+	
 	public CreditCard(String owner, String number) {
 		this.owner = owner;
 		this.number = number;
 	}
-
+	
+	/* getters */
+	
 	public String number() {
 		return this.number;
 	}
+	
 	
 	public double credit() {
 		return this.credit;
 	}
 	
+	/* Comprueba si persona tiene credito para pagar
+	 * si tiene devuelve true
+	 * si no tiene devuelve false */
+	
 	public boolean pay(double creditapagar) {
-		if (credit() - creditapagar != credit) {
+		if (credit() != 0) {
 			this.credit -= creditapagar;
 			return true;
-		}
+		}else {}
 		return false;
 	}
 	
