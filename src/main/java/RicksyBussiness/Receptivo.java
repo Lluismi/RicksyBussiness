a041f9577340dc458bff69fc6e3c623994cc3849
Lluismi <lmorey@cifpfbmoll.eu>
2020-02-27 13:26:16 +0100

    Añadido codigo de la logica

--- src/main/java/RicksyBussiness/Receptivo.java
+++ src/main/java/RicksyBussiness/Receptivo.java
@@ -5,17 +5,23 @@ import java.util.List;
 
 public class Receptivo {
 	
-	List<String> observers = new ArrayList<String>();
+	List<GuestDispatcher> observers = new ArrayList<GuestDispatcher>();
 	
 	/* Constructor */
 	
 	public Receptivo() {}
-
-	public void registra(GuestDispatcher GuestDispatcher) {
-		
+	
+	/* Añade el pack a la lista observers */
+	
+	public void registra(GuestDispatcher packbievenida) {
+		observers.add(packbievenida);
 	}
 	
+	/* Por cada persona despachada se le da un pack de bienvenida */
+	
 	public void dispatch(CreditCard persona) {
-		
+		for (GuestDispatcher personainvitada : observers) {
+			personainvitada.dispatch(persona);
+		}
 	}
 }
